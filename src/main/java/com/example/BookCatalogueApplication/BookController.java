19fd687baab156a1e1b1993794ad97d7afd74723
NathanTNguyen <nathannguyen1590@gmail.com>
2023-07-12 21:27:46 +1000

    comments + search by author name feature

--- src/main/java/com/example/BookCatalogueApplication/BookController.java
+++ src/main/java/com/example/BookCatalogueApplication/BookController.java
@@ -9,11 +9,13 @@ import java.util.stream.Collectors;
 public class BookController {
     private final List<Book> books = new ArrayList<>();
 
+    //get all books
     @GetMapping("/books")
     public List<Book> getAllBooks() {
         return books;
     }
 
+    //search books by book name
     @GetMapping("/books/search")
     public List<Book> searchBookName(@RequestParam String bookName) {
         return books.stream()
@@ -21,17 +23,28 @@ public class BookController {
                 .collect(Collectors.toList());
     }
 
+    //search books by author name
+    @GetMapping("/books/search")
+    public List<Book> searchAuthor(@RequestParam String authorName) {
+        return books.stream()
+                .filter(book -> book.getAuthor().toLowerCase().contains(authorName.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    //add book feature
     @PostMapping("/books")
     public void addBook(@RequestBody Book newBook) {
         books.add(newBook);
     }
 
+    //edit book information by ID identification
     @PutMapping("/books/{id}")
     public void editBook(@PathVariable int id, @RequestBody Book updatedBook) {
         books.removeIf(book -> book.getId() == id);
         books.add(updatedBook);
     }
 
+    //delete book by id
     @DeleteMapping("/books/{id}")
     public void deleteBook(@PathVariable int id) {
         books.removeIf(book -> book.getId() == id);
